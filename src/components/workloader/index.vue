bf256024d3b18944b9ad15792e78974e26315f51
ziwen fan <zfan40@gatech.edu>
2018-06-26 00:11:56 +0800

    upgrade block (cleaner tinyer, yet stronger)

---
+++ src/components/workloader/index.vue
@@ -0,0 +1,45 @@
+<template>
+<el-select v-model="value8" filterable placeholder="Select" @change="change">
+  <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+  </el-option>
+</el-select>
+</template>
+
+<script>
+import Blockly from "node-blockly/browser"; // import Blockly
+import {test1,mario,ff} from "@/util/demos"
+export default {
+  data() {
+    return {
+      options: [{
+        value: 'test',
+        label: 'test'
+      }, {
+        value: 'mario',
+        label: 'mario'
+      }, {
+        value: 'ff',
+        label: 'ff'
+      }],
+      value8:''
+    }
+  },
+  methods: {
+    change(e) {
+      console.log(e)
+      const a = {test1,mario,ff}
+      this.loadWork(a[e])
+    },
+    loadWork(blocklyXmlText) {
+      let workspace = Blockly.getMainWorkspace();
+      workspace.clear();
+      if (blocklyXmlText) {
+        Blockly.Xml.domToWorkspace(
+          Blockly.Xml.textToDom(blocklyXmlText),
+          workspace
+        ); // 把xml dom放到workspace里头展示出来
+      }
+    },
+  }
+}
+</script>
--- src/util/blockly/musixise.js
+++ src/util/blockly/musixise.js
@@ -15,6 +15,311 @@
  * limitations under the License.
  */
 import Blockly from "node-blockly/browser";
+
+/***************** new methods (simpler)*********************/
+Blockly.defineBlocksWithJsonArray([
+  // Block for colour picker.
+  {
+    type: "create_track",
+    message0: "乐轨 音色 %1 速度 %2 音量 %3",
+    args0: [
+      {
+        type: "input_value",
+        name: "TIMBRE",
+        check: "String"
+      },
+      {
+        type: "input_value",
+        name: "TEMPO",
+        check: "Number"
+      },
+      {
+        type: "input_value",
+        name: "VOLUMN",
+        check: "Number" //should be array
+      }
+    ],
+    message1: "小节 %1",
+    args1: [{ type: "input_statement", name: "measures" }],
+    inputsInline: true,
+    previousStatement: null,
+    nextStatement: null,
+    colour: 155,
+    tooltip: "",
+    helpUrl: ""
+  }
+]);
+
+Blockly.JavaScript["create_track"] = function(block) {
+  // Do while/until loop.
+  const timbre = Blockly.JavaScript.valueToCode(
+    block,
+    "TIMBRE",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  const measure = Blockly.JavaScript.valueToCode(
+    block,
+    "TEMPO",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  const volumn = Blockly.JavaScript.valueToCode(
+    block,
+    "VOLUMN",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  var branch = Blockly.JavaScript.statementToCode(block, "measures");
+  // branch = Blockly.JavaScript.addLoopTrap(branch, block.id);
+  console.log("=====", timbre);
+  console.log("=====", measure);
+  console.log("=====", volumn);
+  console.log("=====", branch);
+  return `
+  createTrack(${timbre},${measure},${volumn});
+  ${branch}
+  `;
+};
+
+Blockly.defineBlocksWithJsonArray([
+  // Block for colour picker.
+  {
+    type: "create_measure_new",
+    message0: "音符对位 小节 %1 音序 %2 节拍%3",
+    args0: [
+      {
+        type: "input_value",
+        name: "MEASURE",
+        check: "Number"
+      },
+      {
+        type: "input_value",
+        name: "SEQUENCE",
+        check: "String" //should be array
+      },
+      {
+        type: "input_value",
+        name: "BEAT",
+        check: "String"
+      }
+    ],
+    inputsInline: true,
+    previousStatement: null,
+    nextStatement: null,
+    colour: 355,
+    tooltip: "",
+    helpUrl: ""
+  }
+]);
+
+Blockly.JavaScript["create_measure_new"] = function(block) {
+  const measure = Blockly.JavaScript.valueToCode(
+    block,
+    "MEASURE",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  const sequence = Blockly.JavaScript.valueToCode(
+    block,
+    "SEQUENCE",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  const beat = Blockly.JavaScript.valueToCode(
+    block,
+    "BEAT",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  return `createMeasureNew(${measure},${sequence},${beat});\n`;
+};
+
+Blockly.defineBlocksWithJsonArray([
+  // Block for colour picker.
+  {
+    type: "create_measure_on_scale_new",
+    message0: "简谱对位 小节%1 音序 %2 节拍%3 调式%4 根音%5",
+    args0: [
+      {
+        type: "input_value",
+        name: "MEASURE",
+        check: "Number"
+      },
+      {
+        type: "input_value",
+        name: "SEQUENCE",
+        check: "String" //should be array
+      },
+      {
+        type: "input_value",
+        name: "BEAT",
+        check: "String"
+      },
+      {
+        type: "input_value",
+        name: "SCALE",
+        check: "String"
+      },
+      {
+        type: "input_value",
+        name: "BASENOTE",
+        check: "String"
+      }
+    ],
+    inputsInline: true,
+    previousStatement: null,
+    nextStatement: null,
+    colour: 355,
+    tooltip: "",
+    helpUrl: ""
+  }
+]);
+
+Blockly.JavaScript["create_measure_on_scale_new"] = function(block) {
+  const measure = Blockly.JavaScript.valueToCode(
+    block,
+    "MEASURE",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  const sequence = Blockly.JavaScript.valueToCode(
+    block,
+    "SEQUENCE",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  const beat = Blockly.JavaScript.valueToCode(
+    block,
+    "BEAT",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  const scale = Blockly.JavaScript.valueToCode(
+    block,
+    "SCALE",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  const basenote = Blockly.JavaScript.valueToCode(
+    block,
+    "BASENOTE",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  return `createMeasureOnScaleNew(${measure},${sequence},${beat},${scale},${basenote});\n`;
+};
+
+Blockly.defineBlocksWithJsonArray([
+  // Block for colour picker.
+  {
+    type: "create_measure_match_zero_new",
+    message0: "音符不对位 小节 %1 音序 %2 节拍%3",
+    args0: [
+      {
+        type: "input_value",
+        name: "MEASURE",
+        check: "Number"
+      },
+      {
+        type: "input_value",
+        name: "SEQUENCE",
+        check: "String" //should be array
+      },
+      {
+        type: "input_value",
+        name: "BEAT",
+        check: "String"
+      }
+    ],
+    inputsInline: true,
+    previousStatement: null,
+    nextStatement: null,
+    colour: 355,
+    tooltip: "",
+    helpUrl: ""
+  }
+]);
+
+Blockly.JavaScript["create_measure_match_zero_new"] = function(block) {
+  const measure = Blockly.JavaScript.valueToCode(
+    block,
+    "MEASURE",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  const sequence = Blockly.JavaScript.valueToCode(
+    block,
+    "SEQUENCE",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  const beat = Blockly.JavaScript.valueToCode(
+    block,
+    "BEAT",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  return `createMeasureNew(${measure},${sequence},${beat},true);\n`;
+};
+
+Blockly.defineBlocksWithJsonArray([
+  // Block for colour picker.
+  {
+    type: "create_measure_on_scale_match_zero_new",
+    message0: "简谱不对位 小节 %1 音序 %2 节拍%3 调式%4 根音%5",
+    args0: [
+      {
+        type: "input_value",
+        name: "MEASURE",
+        check: "Number"
+      },
+      {
+        type: "input_value",
+        name: "SEQUENCE",
+        check: "String" //should be array
+      },
+      {
+        type: "input_value",
+        name: "BEAT",
+        check: "String"
+      },
+      {
+        type: "input_value",
+        name: "SCALE",
+        check: "String"
+      },
+      {
+        type: "input_value",
+        name: "BASENOTE",
+        check: "String"
+      }
+    ],
+    inputsInline: true,
+    previousStatement: null,
+    nextStatement: null,
+    colour: 355,
+    tooltip: "",
+    helpUrl: ""
+  }
+]);
+
+Blockly.JavaScript["create_measure_on_scale_match_zero_new"] = function(block) {
+  const measure = Blockly.JavaScript.valueToCode(
+    block,
+    "MEASURE",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  const sequence = Blockly.JavaScript.valueToCode(
+    block,
+    "SEQUENCE",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  const beat = Blockly.JavaScript.valueToCode(
+    block,
+    "BEAT",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  const scale = Blockly.JavaScript.valueToCode(
+    block,
+    "SCALE",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  const basenote = Blockly.JavaScript.valueToCode(
+    block,
+    "BASENOTE",
+    Blockly.JavaScript.ORDER_NONE
+  );
+  return `createMeasureOnScaleNew(${measure},${sequence},${beat},${scale},${basenote},true);\n`;
+};
+
+/*********** old methods (redundant)**************/
 Blockly.defineBlocksWithJsonArray([
   // Block for colour picker.
   {
